b8b7456b833f389cd088c42e402f06cb1851d994
John Koisch <jk@youcentric.com>
2014-12-03 13:25:57 -0800

    documentation

--- app/models/cloud9/opportunity.rb
+++ app/models/cloud9/opportunity.rb
@@ -1,2 +1,4 @@
-  class Cloud9::Opportunity < Hashie::Mash
-  end
+# The Cloud9::Opportunity is just a mapping class from the hash provided by Salesforce. We get most of that for free
+# by using Hashie.
+class Cloud9::Opportunity < Hashie::Mash
+end
