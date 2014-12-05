d2e051ca297f4820dc138518f6b128926f843731
John Koisch <jk@youcentric.com>
2014-12-05 10:07:29 -0800

    cleaned up measurements for the front end

--- app/serializers/measurement_serializer.rb
+++ app/serializers/measurement_serializer.rb
@@ -1,3 +1,3 @@
 class MeasurementSerializer < ActiveModel::Serializer
-  attributes :id, :system_id, :ram, :cpu, :hard_drive_space, :created_at
+  attributes :id, :system_id, :ram, :cpu, :created_at, :average_ram_utilization, :total_hd_space, :free_hd_space, :total_users, :pagefile_size, :dataserver, :datastore_location, :pagefile_location
 end
