656356232d7ea71985c44d2e4039170fc42fef4b
John Koisch <jk@youcentric.com>
2014-12-09 14:56:49 -0800

    supporting the automation workflow for accounts, including finding by account_number

--- app/models/salesforce/account.rb
+++ app/models/salesforce/account.rb
@@ -4,9 +4,17 @@ module Salesforce
 
     include ActiveModel::Model
 
-    attr_accessor :id
+    attr_accessor :id, :account_number
 
     define_model_callbacks :initialize, only: [:after]
+
+    def find
+      unless self.account_number.blank?
+        accounts = @client.query("select Id from Account where AccountNumber = '#{self.account_number}'")
+        self.id = accounts.first.Id
+        salesforce_data
+      end
+    end
 
     def salesforce_data
       @data ||= @client.account(self.id.to_s)
